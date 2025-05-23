b33bf03b977f311f5607290150cba9a961e843f9
Naveena.K <119483016+naveenakirubakaran@users.noreply.github.com>
2025-05-23 16:26:36 +0530

    Create 17. Class and Object Creation.java

---
+++ Core Java Exercises/17. Class and Object Creation.java
@@ -0,0 +1,28 @@
+class Car {
+    String make;
+    String model;
+    int year;
+
+    void displayDetails() {
+        System.out.println("Make: " + make);
+        System.out.println("Model: " + model);
+        System.out.println("Year: " + year);
+    }
+}
+
+public class CarDemo {
+    public static void main(String[] args) {
+        Car car1 = new Car();
+        car1.make = "Toyota";
+        car1.model = "Corolla";
+        car1.year = 2020;
+
+        car1.displayDetails();
+    }
+}
+
+
+## Output
+  Make: Toyota
+  Model: Corolla
+  Year: 2020
