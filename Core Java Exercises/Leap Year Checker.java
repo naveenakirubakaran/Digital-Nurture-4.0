fe9e4f68fcfb8948bce7bc541e8b8e37633f92b0
Naveena.K <119483016+naveenakirubakaran@users.noreply.github.com>
2025-05-23 16:00:41 +0530

    Create Leap Year Checker.java

---
+++ Core Java Exercises/Leap Year Checker.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a year: ");
+        int year = sc.nextInt();
+
+        boolean isLeap = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+
+        if (isLeap)
+            System.out.println(year + " is a leap year.");
+        else
+            System.out.println(year + " is not a leap year.");
+    }
+}
+
+##Output
+  Enter a year: 2024
+  2024 is a leap year.
