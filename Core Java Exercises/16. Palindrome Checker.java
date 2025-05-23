28632b98ff80703f2995f555f9c5678971b00747
Naveena.K <119483016+naveenakirubakaran@users.noreply.github.com>
2025-05-23 16:21:28 +0530

    Create 16. Palindrome Checker.java

---
+++ Core Java Exercises/16. Palindrome Checker.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class PalindromeChecker {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String original = sc.nextLine();
+
+        String cleaned = original.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        String reversed = new StringBuilder(cleaned).reverse().toString();
+
+        if (cleaned.equals(reversed)) {
+            System.out.println("The string is a palindrome.");
+        } else {
+            System.out.println("The string is NOT a palindrome.");
+        }
+    }
+}
+
+## Output
+  Enter a string: Madam
+  The string is a palindrome.
