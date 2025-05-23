1e6e0cd6e8383c1417f838db76042ba309f96960
Naveena.K <119483016+naveenakirubakaran@users.noreply.github.com>
2025-05-23 16:02:06 +0530

    Create Multiplication Table.java

---
+++ Core Java Exercises/Multiplication Table.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " x " + i + " = " + (num * i));
+        }
+    }
+}
+
+##Output
+  Enter a number: 3
+3 x 1 = 3
+3 x 2 = 6
+3 x 3 = 9
+3 x 4 = 12
+3 x 5 = 15
+3 x 6 = 18
+3 x 7 = 21
+3 x 8 = 24
+3 x 9 = 27
+3 x 10 = 30
