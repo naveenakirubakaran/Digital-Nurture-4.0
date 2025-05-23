a3b097cef8be25680d276e9bf39476a5dfa1e9f1
Naveena.K <119483016+naveenakirubakaran@users.noreply.github.com>
2025-05-23 16:03:52 +0530

    Create Data Type Demonstration.java

---
+++ Core Java Exercises/Data Type Demonstration.java
@@ -0,0 +1,22 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int age = 25;
+        float height = 5.9f;
+        double weight = 65.5;
+        char grade = 'A';
+        boolean isStudent = true;
+
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Weight: " + weight);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Student: " + isStudent);
+    }
+}
+
+##Output
+  Age: 25
+Height: 5.9
+Weight: 65.5
+Grade: A
+Is Student: true
