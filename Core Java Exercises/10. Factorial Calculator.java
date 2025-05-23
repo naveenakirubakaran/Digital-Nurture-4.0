b3eb11d8f681d1a3d8638a63f97452afca730c41
Naveena.K <119483016+naveenakirubakaran@users.noreply.github.com>
2025-05-23 16:13:52 +0530

    Create 10. Factorial Calculator.java

---
+++ Core Java Exercises/10. Factorial Calculator.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a non-negative integer: ");
+        int num = sc.nextInt();
+        long fact = 1;
+
+        for (int i = 1; i <= num; i++) {
+            fact *= i;
+        }
+
+        System.out.println("Factorial of " + num + " is: " + fact);
+    }
+}
+
+
+## Output
+Enter a non-negative integer: 5
+Factorial of 5 is: 120
