ecbeadf7097e346dcd94efd69769b570b9541de4
Naveena.K <119483016+naveenakirubakaran@users.noreply.github.com>
2025-05-25 16:10:01 +0530

    Update Completed Events with Feedback Summary.sql

--- Mysql/Completed Events with Feedback Summary.sql
+++ Mysql/Completed Events with Feedback Summary.sql
@@ -6,3 +6,8 @@ LEFT JOIN Registrations r ON e.event_id = r.event_id
 LEFT JOIN Feedback f ON e.event_id = f.event_id
 WHERE e.status = 'completed'
 GROUP BY e.event_id;
+
+Output
+| event_id  | event_title        | total_registrations | avg_rating |
+| --------- | ------------------ | -------------------- | ----------- |
+| 2         | AI & ML Conference | 2                    | 4.5         |
