f8e57dacdb7c3ba3844c3f65392b44eb09f406ed
Naveena.K <119483016+naveenakirubakaran@users.noreply.github.com>
2025-05-25 16:11:46 +0530

    Update Top Feedback Providers.sql

--- Mysql/Top Feedback Providers.sql
+++ Mysql/Top Feedback Providers.sql
@@ -4,3 +4,12 @@ JOIN Feedback f ON u.user_id = f.user_id
 GROUP BY u.user_id
 ORDER BY total_feedbacks DESC
 LIMIT 5;
+
+
+Output
+
+| user_id  | full_name   | feedback_count |
+| -------- | ----------- | --------------- |
+| 2        | Bob Smith   | 1               |
+| 3        | Charlie Lee | 1               |
+| 4        | Diana King  | 1               |
