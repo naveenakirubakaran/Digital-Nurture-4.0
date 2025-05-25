054b4f7a57d3abed0a7f6d875c2e64372186f700
Naveena.K <119483016+naveenakirubakaran@users.noreply.github.com>
2025-05-25 16:08:00 +0530

    Update Unregistered Active Users.sql

--- Mysql/Unregistered Active Users.sql
+++ Mysql/Unregistered Active Users.sql
@@ -2,3 +2,7 @@ SELECT *
 FROM Users
 WHERE registration_date >= CURDATE() - INTERVAL 30 DAY
 AND user_id NOT IN (SELECT user_id FROM Registrations);
+
+Output
+| user\_id  | full\_name | registration\_date |
+
