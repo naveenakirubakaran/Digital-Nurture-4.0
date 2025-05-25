ebec39c43bcc984259e58b6b28c1def4fcdfded7
Naveena.K <119483016+naveenakirubakaran@users.noreply.github.com>
2025-05-25 16:06:36 +0530

    Update Most Registered Events.sql

--- Mysql/Most Registered Events.sql
+++ Mysql/Most Registered Events.sql
@@ -4,3 +4,10 @@ JOIN Registrations r ON e.event_id = r.event_id
 GROUP BY e.event_id
 ORDER BY total_registrations DESC
 LIMIT 3;
+
+Output
+| event_id  | event_title            | total_registrations |
+| --------- | ---------------------- | -------------------- |
+| 1         | Tech Innovators Meetup | 2                    |
+| 2         | AI & ML Conference     | 2                    |
+| 3         | Frontend Bootcamp      | 1                    |
