fded1fa6bec2fa9d759d8ab6ede8dd0f25f4acd3
Naveena.K <119483016+naveenakirubakaran@users.noreply.github.com>
2025-05-25 15:57:59 +0530

    Update Most Active Cities.sql

--- Mysql/Most Active Cities.sql
+++ Mysql/Most Active Cities.sql
@@ -4,3 +4,10 @@ JOIN Registrations r ON u.user_id = r.user_id
 GROUP BY city
 ORDER BY user_count DESC
 LIMIT 5;
+
+
+| city        | distinct_user_registrations |
+| ----------- | ----------------------------- |
+| New York    | 2                             |
+| Los Angeles | 2                             |
+| Chicago     | 1                             |
