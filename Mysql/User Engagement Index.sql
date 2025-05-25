b1b0adcaa69a875940a36e520a7081bc980c45c4
Naveena.K <119483016+naveenakirubakaran@users.noreply.github.com>
2025-05-25 16:10:37 +0530

    Update User Engagement Index.sql

--- Mysql/User Engagement Index.sql
+++ Mysql/User Engagement Index.sql
@@ -5,3 +5,14 @@ FROM Users u
 LEFT JOIN Registrations r ON u.user_id = r.user_id
 LEFT JOIN Feedback f ON u.user_id = f.user_id
 GROUP BY u.user_id;
+
+
+Output
+
+| user\_id | full\_name    | events\_attended | feedbacks\_given |
+| -------- | ------------- | ---------------- | ---------------- |
+| 1        | Alice Johnson | 1                | 0                |
+| 2        | Bob Smith     | 1                | 1                |
+| 3        | Charlie Lee   | 1                | 1                |
+| 4        | Diana King    | 1                | 1                |
+| 5        | Ethan Hunt    | 1                | 0                |
